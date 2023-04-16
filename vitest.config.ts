b8dfc9c6dc70a15e7898e95bc289aaabde86a4d0
Lluís Ulzurrun de Asanza i Sàez <me@llu.is>
2023-04-16 08:38:20 +0200

    :bug: Fix code coverage reports

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
-      reporter: ['text', 'lcov', 'html'],
+      provider: 'c8',
+      reporter: ['lcov', 'text', 'html'],
     },
   },
 });
